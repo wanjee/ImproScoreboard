a596c5aa393a2fbfe1f0a5f52fc18d1690adf499
Sébastien Piraux <sebastien_piraux@epam.com>
2024-02-01 14:12:24 +0100

    Add base path for deployment to Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,19 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const base = (mode === 'development') ? '' : "/ImproScoreboard/";
+
+  return {
+    base: base,
+    plugins: [
+      vue(),
+      vuetify(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
