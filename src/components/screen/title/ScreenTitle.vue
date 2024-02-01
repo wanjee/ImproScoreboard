ed79068f0608b116e97c42482085050cd832ca1f
Sébastien Piraux <sebastien_piraux@epam.com>
2024-02-01 10:30:14 +0100

    Add temporary show logo and message state props display to title screen

--- src/components/screen/title/ScreenTitle.vue
+++ src/components/screen/title/ScreenTitle.vue
@@ -12,6 +12,16 @@ const titleScreenStore = useTitleScreenStore()
         <h2 v-if="titleScreenStore.secondaryTitle">{{ titleScreenStore.secondaryTitle }}</h2>
       </v-col>
     </v-row>
+    <v-row v-if="titleScreenStore.showLogo" class="d-flex justify-start align-center pa-6">
+      <v-col class="d-flex flex-column justify-center align-center">
+        Logo here
+      </v-col>
+    </v-row>
+    <v-row v-if="titleScreenStore.message" class="d-flex justify-start align-center pa-6">
+      <v-col class="d-flex flex-column justify-center align-center">
+        {{ titleScreenStore.message }}
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
