86a51ad583e28cae5ac948878248a7d7ebbf45e3
Sébastien Piraux <sebastien_piraux@epam.com>
2024-02-04 13:52:09 +0100

    WIP on timer control: attempt to get it to stop at the end of countdown; reset when duration changes
    FIXME: seconds stops, minutes don't; allow restart when duration does not change

--- src/components/theTimer.vue
+++ src/components/theTimer.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
 import { useTimerStore } from '@/stores/timer'
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
+import { storeToRefs } from 'pinia'
+
+const clockElement = ref<HTMLOListElement | null>(null)
 
 const timerStore = useTimerStore()
+const { duration } = storeToRefs(timerStore)
 
-/* CSS timer logic using CSS animations mostly comes
-   from https://dev.to/madsstoumann/clocks-countdowns-timing-in-css-and-javascript-554n
-*/
+/**
+ * Watch duration so that we can reset animation when updated
+ */
+watch(duration, () => {
+  // Remove animation class
+  clockElement.value?.classList.remove('animated')
+  // The actual magic to reset the animation -> triggering reflow
+  // without this it wouldn't work.
+  void clockElement.value?.offsetWidth
+
+  // Add animation class back
+  clockElement.value?.classList.add('animated')
+})
+
+/**
+ * CSS timer logic using CSS animations mostly comes
+ *  from https://dev.to/madsstoumann/clocks-countdowns-timing-in-css-and-javascript-554n
+ */
 const cssProps = computed(() => {
-  const remaining = timerStore.duration * 1000 // in milliseconds
+  const remaining = timerStore.duration * 1000 // FIXME use seconds directly in milliseconds
 
   // Periods to seconds
   const MINUTE = 60
@@ -17,6 +36,7 @@ const cssProps = computed(() => {
 
   // We use seconds only
   // Split the remaining time to each time part we need to display, in the displayed unit
+  const remainingTotal = Math.floor(remaining / 1000)
   const remainingSeconds = Math.floor((remaining / 1000) % 60)
   const remainingMinutes = Math.floor((remaining / (60 * 1000)) % 60) * MINUTE
   const remainingHours = Math.floor((remaining / (HOUR * 1000)) % 24) * HOUR
@@ -30,13 +50,19 @@ const cssProps = computed(() => {
     '--delay-seconds': -delaySeconds + 's',
     '--delay-minutes': -delayMinutes + 's',
     '--delay-hours': -delayHours + 's',
+    // one iteration covers 60s / one minute
+    '--iterations-seconds': Math.ceil(remainingTotal / MINUTE) + 1,
+    // one iteration covers 60m / one hour
+    '--iterations-minutes': Math.ceil(remainingTotal / HOUR) + 1,
+    // one iteration covers 24h / one day
+    '--iterations-hours': Math.ceil(remainingTotal / DAY) + 1,
     '--play-state': timerStore.isRunning ? 'running' : 'paused',
   }
 })
 </script>
 
 <template>
-  <ol class="clock" :style="cssProps">
+  <ol class="clock animated" :style="cssProps" ref="clockElement">
     <li v-if="timerStore.showHours" class="time-part hours"></li>
     <li class="time-part minutes"></li>
     <li class="time-part seconds"></li>
@@ -62,9 +88,12 @@ const cssProps = computed(() => {
 
 .clock {
   /* Declare variables that will be overridden on mount */
-  --delay-hours: '0s';
-  --delay-minutes: '0s';
   --delay-seconds: '0s';
+  --delay-minutes: '0s';
+  --delay-hours: '0s';
+  --iterations-seconds: '0';
+  --iterations-minutes: '0';
+  --iterations-hours: '0';
   --play-state: paused;
 
   all: unset;
@@ -87,32 +116,57 @@ const cssProps = computed(() => {
     padding: 0 10px;
     text-align: center;
     width: auto;
+  }
 
-    &.hours {
-      animation: hours 86400s steps(24, jump-none) infinite;
-      animation-delay: var(--delay-hours, 0s);
-      animation-play-state: var(--play-state);
-      counter-reset: hours var(--hours);
-      &::after {
-        content: counter(hours, decimal-leading-zero) ' ';
+  &.animated {
+    & li {
+      &.seconds {
+        animation-name: seconds;
+        animation-duration: 60s;
+        animation-timing-function: steps(60, jump-none);
+        /* ensure it starts at the correct frame */
+        animation-delay: var(--delay-seconds, 0s);
+        /* play/pause */
+        animation-play-state: var(--play-state);
+        /* stop when all iterations are done */
+        animation-iteration-count: var(--iterations-seconds, 1);
+        /* get and display the value */
+        counter-reset: seconds var(--seconds);
+        &::after {
+          content: counter(seconds, decimal-leading-zero) ' ';
+        }
       }
-    }
-    &.minutes {
-      animation: minutes 3600s steps(60, jump-none) infinite;
-      animation-delay: var(--delay-minutes, 0s);
-      animation-play-state: var(--play-state);
-      counter-reset: minutes var(--minutes);
-      &::after {
-        content: counter(minutes, decimal-leading-zero) ' ';
+      &.minutes {
+        animation-name: minutes;
+        animation-duration: 3600s;
+        animation-timing-function: steps(60, jump-none);
+        /* ensure it starts at the correct frame */
+        animation-delay: var(--delay-minutes, 0s);
+        /* play/pause */
+        animation-play-state: var(--play-state);
+        /* stop when all iterations are done */
+        animation-iteration-count: var(--iterations-minutes, 1);
+        /* get and display the value */
+        counter-reset: minutes var(--minutes);
+        &::after {
+          content: counter(minutes, decimal-leading-zero) ' ';
+        }
       }
-    }
-    &.seconds {
-      animation: seconds 60s steps(60, jump-none) infinite;
-      animation-delay: var(--delay-seconds, 0s);
-      animation-play-state: var(--play-state);
-      counter-reset: seconds var(--seconds);
-      &::after {
-        content: counter(seconds, decimal-leading-zero) ' ';
+      &.hours {
+        animation-name: hours;
+        animation-duration: 86400s;
+        animation-timing-function: steps(24, jump-none);
+        /* ensure it starts at the correct frame */
+        animation-delay: var(--delay-hours, 0s);
+        /* play/pause */
+        animation-play-state: var(--play-state);
+        /* stop when all iterations are done */
+        animation-iteration-count: var(--iterations-hours, 1);
+        /* get and display the value */
+        counter-reset: hours var(--hours);
+        &::after {
+          content: counter(hours, decimal-leading-zero) ' ';
+        }
       }
     }
   }
