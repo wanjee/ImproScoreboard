6ae3aeb6a089e8e345eef4e8a75e8909a4799505
Sébastien Piraux <sebastien_piraux@epam.com>
2024-02-06 14:59:01 +0100

    Remove theme switcher, keep it always dark so you have no risk to have a big light visible during a show

--- src/views/ControlView.vue
+++ src/views/ControlView.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
 import { useScoreStore } from '@/stores/score'
-import { useTheme } from 'vuetify'
 import ControlTeam from '@/components/control/team/ControlTeam.vue'
 import { storeToRefs } from 'pinia'
 import ControlReset from '@/components/control/reset/ControlReset.vue'
 import ControlSettings from '@/components/control/settings/ControlSettings.vue'
 import ControlTimer from '@/components/control/timer/ControlTimer.vue'
 
-const theme = useTheme()
 const scoreStore = useScoreStore()
 
 const { teams } = storeToRefs(scoreStore)
-
-// Globally switch the admin from light to dark theme
-function toggleTheme() {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-}
 </script>
 
 <template>
@@ -24,7 +17,6 @@ function toggleTheme() {
       <template v-slot:prepend>
         <ControlSettings></ControlSettings>
         <ControlReset></ControlReset>
-        <v-btn icon="mdi-theme-light-dark" elevation="2" @click="toggleTheme" title="Switch theme (dark/light)"></v-btn>
       </template>
       <v-app-bar-title>La Ligue d'Impro - Control</v-app-bar-title>
       <v-spacer></v-spacer>
