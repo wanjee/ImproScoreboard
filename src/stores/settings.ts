77221daec5761dbe97f221a27798190049793083
Sébastien Piraux <sebastien_piraux@epam.com>
2025-01-27 16:50:55 +0100

    Update default message font size and margin settings to something that fits "La Sucrerie" screen setup a bit more

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -6,21 +6,21 @@ export const useSettingsStore = defineStore('settings', {
       dimmerOpacity: 0, // %
       margins: {
         top: 0, // vh
-        right: 0, // vw
+        right: 5, // vw
         bottom: 0, // vh
-        left: 0, // vw
+        left: 5, // vw
       },
-      messageFontSize: 3.5, // vh
+      messageFontSize: 4, // vh
     }
   },
   actions: {
     reset() {
       this.dimmerOpacity = 0 // %
       this.margins.top = 0 // vh
-      this.margins.right = 0 // vw
+      this.margins.right = 5 // vw
       this.margins.bottom = 0 // vh
-      this.margins.left = 0 // vw
-      this.messageFontSize = 3.5 // vh
+      this.margins.left = 5 // vw
+      this.messageFontSize = 4 // vh
     },
   },
 })
